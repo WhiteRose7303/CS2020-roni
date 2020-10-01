37ab8adcddb66666b4bd3767cc23a2949cc18cf9
hadar ovadia <hadarov@tichonet.com>
2020-10-01 16:55:38 +0300

    new timer and shit

--- Form_Memo/Form1.Designer.cs
+++ Form_Memo/Form1.Designer.cs
@@ -28,12 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -45,80 +47,92 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Form_Memo.Properties.Resources.Back;
-            this.pictureBox1.Location = new System.Drawing.Point(66, 51);
+            this.pictureBox1.Location = new System.Drawing.Point(33, 27);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(237, 129);
+            this.pictureBox1.Size = new System.Drawing.Size(118, 67);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox_Card_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Form_Memo.Properties.Resources.Back;
-            this.pictureBox2.Location = new System.Drawing.Point(431, 51);
+            this.pictureBox2.Location = new System.Drawing.Point(216, 27);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(237, 129);
+            this.pictureBox2.Size = new System.Drawing.Size(118, 67);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox_Card_Click);
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::Form_Memo.Properties.Resources.Back;
-            this.pictureBox3.Location = new System.Drawing.Point(66, 251);
+            this.pictureBox3.Location = new System.Drawing.Point(33, 131);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(237, 129);
+            this.pictureBox3.Size = new System.Drawing.Size(118, 67);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox_Card_Click);
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = global::Form_Memo.Properties.Resources.Back;
-            this.pictureBox4.Location = new System.Drawing.Point(431, 251);
+            this.pictureBox4.Location = new System.Drawing.Point(216, 131);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(237, 129);
+            this.pictureBox4.Size = new System.Drawing.Size(118, 67);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
-            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox_Card_Click);
             // 
             // pictureBox5
             // 
             this.pictureBox5.Image = global::Form_Memo.Properties.Resources.Back;
-            this.pictureBox5.Location = new System.Drawing.Point(66, 455);
+            this.pictureBox5.Location = new System.Drawing.Point(33, 237);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(237, 129);
+            this.pictureBox5.Size = new System.Drawing.Size(118, 67);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
-            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox_Card_Click);
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = global::Form_Memo.Properties.Resources.Back;
-            this.pictureBox6.Location = new System.Drawing.Point(431, 455);
+            this.pictureBox6.Location = new System.Drawing.Point(216, 237);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(237, 129);
+            this.pictureBox6.Size = new System.Drawing.Size(118, 67);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 5;
             this.pictureBox6.TabStop = false;
-            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox_Card_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(762, 675);
+            this.ClientSize = new System.Drawing.Size(370, 339);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -139,6 +153,7 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form_Memo/Form1.cs
+++ Form_Memo/Form1.cs
@@ -16,102 +16,98 @@ namespace Form_Memo
         public Form1()
         {
             InitializeComponent();
+            setimagearray();
         }
-        private bool m_IsBack1 = true;
-        private bool m_IsBack2 = true;
-        private bool m_IsBack3 = true;
-        private bool m_IsBack4 = true;
-        private bool m_IsBack5 = true;
-        private bool m_IsBack6 = true;
+        
+        private bool m_IsFirst = true;
+        private PictureBox m_FirstPictureBox;
+        private PictureBox m_SecondPictureBox;
+        
+        private const int Card_Number = 6;
+        private Image[] m_Images = new Image[Card_Number];
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        public void setimagearray()
         {
-            if (!m_IsBack1)
+            m_Images[0] = (Resources.supra_1);
+            m_Images[1] = (Resources.supra_2);
+            m_Images[2] = (Resources.divo_1);
+            m_Images[3] = (Resources.divo_2);
+            m_Images[4] = (Resources.Phantom_1);
+            m_Images[5] = (Resources.Phantom_2);
+        }
+
+        private void pictureBox_Card_Click(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = sender as PictureBox;
+            string picName = pictureBox.Name;
+            int k = int.Parse(picName.Substring(picName.Length - 1));
+            k--;
+            if (!IsImagesMatch(pictureBox.Image, Resources.Back))
+                pictureBox.Image = Resources.Back;
+            else
+                pictureBox.Image = m_Images[k];
+
+
+            if (!m_IsFirst)
             {
-                pictureBox1.Image = Resources.Back;
-                m_IsBack1 = true;
+                m_SecondPictureBox = pictureBox;
+                
             }
             else
             {
-
-                pictureBox1.Image = Resources.supra_1;
-                m_IsBack1 = false;
+                m_FirstPictureBox = pictureBox;
+                timer1.Start();
             }
+            m_IsFirst = !m_IsFirst;
+
         }
 
-        private void pictureBox2_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            if (!m_IsBack2)
-            {
-                pictureBox2.Image = Resources.Back;
-                m_IsBack2 = true;
-            }
-            else
-            {
-
-                pictureBox2.Image = Resources.supra_2;
-                m_IsBack2 = false;
-            }
+            m_FirstPictureBox.Image = Resources.Back;
+            m_SecondPictureBox.Image = Resources.Back;
+            timer1.Stop();
         }
+       
 
-        private void pictureBox3_Click(object sender, EventArgs e)
+        public bool IsImagesMatch(Image image1, Image image2)
         {
-            if (!m_IsBack3)
+            try
             {
-                pictureBox3.Image = Resources.Back;
-                m_IsBack3 = true;
-            }
-            else
-            {
+                //create instance or System.Drawing.ImageConverter to convert
+                //each image to a byte array
+                ImageConverter converter = new ImageConverter();
+                //create 2 byte arrays, one for each image
+                byte[] imgBytes1 = new byte[1];
+                byte[] imgBytes2 = new byte[1];
 
-                pictureBox3.Image = Resources.Phantom_1;
-                m_IsBack3 = false;
+                //convert images to byte array
+                imgBytes1 = (byte[])converter.ConvertTo(image1, imgBytes2.GetType());
+                imgBytes2 = (byte[])converter.ConvertTo(image2, imgBytes1.GetType());
+
+                //now compute a hash for each image from the byte arrays
+                System.Security.Cryptography.SHA256Managed sha = new System.Security.Cryptography.SHA256Managed();
+                byte[] imgHash1 = sha.ComputeHash(imgBytes1);
+                byte[] imgHash2 = sha.ComputeHash(imgBytes2);
+
+                //now let's compare the hashes
+                for (int i = 0; i < imgHash1.Length && i < imgHash2.Length; i++)
+                {
+                    //whoops, found a non-match, exit the loop
+                    //with a false value
+                    if (!(imgHash1[i] == imgHash2[i]))
+                        return false;
+                }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            //we made it this far so the images must match
+            return true;
         }
 
-        private void pictureBox4_Click(object sender, EventArgs e)
-        {
-            if (!m_IsBack4)
-            {
-                pictureBox4.Image = Resources.Back;
-                m_IsBack4 = true;
-            }
-            else
-            {
-
-                pictureBox4.Image = Resources.Phantom_2;
-                m_IsBack4 = false;
-            }
-        }
-
-        private void pictureBox5_Click(object sender, EventArgs e)
-        {
-            if (!m_IsBack5)
-            {
-                pictureBox5.Image = Resources.Back;
-                m_IsBack5 = true;
-            }
-            else
-            {
-
-                pictureBox5.Image = Resources.divo_1;
-                m_IsBack5 = false;
-            }
-        }
-
-        private void pictureBox6_Click(object sender, EventArgs e)
-        {
-            if (!m_IsBack6)
-            {
-                pictureBox6.Image = Resources.Back;
-                m_IsBack6 = true;
-            }
-            else
-            {
-
-                pictureBox6.Image = Resources.divo_2;
-                m_IsBack6 = false;
-            }
-        }
+       
     }
 }
