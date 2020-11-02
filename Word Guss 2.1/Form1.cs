1455a2b6507c303b315b07e26feb9417ad720ec8
hadar ovadia <hadarov@tichonet.com>
2020-11-02 16:26:37 +0200

    looser dialog inserted

--- Word Guss 2.1/Form1.cs
+++ Word Guss 2.1/Form1.cs
@@ -4,10 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Word_Guss_2.Properties;
 
 namespace Word_Guss_2._1
 {
@@ -16,11 +19,14 @@ namespace Word_Guss_2._1
         public Form1()
         {
             InitializeComponent();
+            SetImagesArray();
         }
 
         private int m_CurrentLabelLetter = 1;
         private string m_WordToGuess = "";
-        private Image[] m_Images = new Image[5];
+        private Image[] m_Images = new Image[6];
+        private int m_CountError = 0;
+        private bool m_IsFirstPlayer = true;
 
         public void SetImagesArray()
         {
@@ -32,15 +38,51 @@ namespace Word_Guss_2._1
             m_Images[4] = Resources.p5;
             m_Images[5] = Resources.p6;
         }
+
         private void button_Letter_Click(object sender, EventArgs e)
         {
             string buttonText = (sender as Button).Text;
-            groupBox2.Controls["label_Letter" + m_CurrentLabelLetter].Text = buttonText;
-           
-            m_CurrentLabelLetter++;
-            if (m_CurrentLabelLetter == 6)
-                button_OK.Enabled = true;
-           
+            if (m_IsFirstPlayer)
+            {
+                groupBox2.Controls["label_Letter" + m_CurrentLabelLetter].Text = buttonText;
+
+                m_CurrentLabelLetter++;
+                if (m_CurrentLabelLetter == 6)
+                    button_OK.Enabled = true;
+            }
+            else
+            {
+                if (m_WordToGuess.Contains(buttonText))
+                {
+                    for (int i = 0; i < m_WordToGuess.Length; i++)
+                        if (m_WordToGuess[i].ToString() == buttonText)
+                            groupBox2.Controls["label_Letter" + (i + 1)].Text = buttonText;
+                }
+                else
+                {
+                    pictureBox1.Visible = true;
+                    //הצגת תמונת שגיאה  
+                    m_CountError++;
+                    pictureBox1.Image = m_Images[m_CountError - 1];
+                }
+                (sender as Button).Enabled = false;
+                if (m_CountError == 6)
+                {
+                    DialogResult dialogResult = MessageBox.Show("Would you like to reset?", "Reset?", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        reset();
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        this.Close();
+                    }
+                }
+
+            }
+
+
+
         }
 
         private void button27_Click(object sender, EventArgs e)
@@ -64,12 +106,36 @@ namespace Word_Guss_2._1
 
             button27.Visible = false;
             button_OK.Visible = false;
+            m_IsFirstPlayer = false;
+
 
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public void reset()
+        {
+            m_CountError = 0;
+            m_IsFirstPlayer = true;
+            pictureBox1.Visible = false;
+            m_CurrentLabelLetter = 1;
+            button_OK.Enabled = true;
+            button_OK.Visible = true;
+            button27.Enabled = true;
+            button27.Visible = true;
+            SetImagesArray();
+            for (int i = 1; i <= 5; i++)
+            {
+                groupBox2.Controls["label_Letter" + (i)].Text = "_";
+            }
+            for (int i = 1; i<= 26; i++)
+            {
+                groupBox1.Controls["button" + (i)].Enabled = true;
+            }
+            
         }
     }
     
--- Word Guss 2.1/Properties/Resources.Designer.cs
+++ Word Guss 2.1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Word_Guss_2._1.Properties {
+namespace Word_Guss_2.Properties {
     using System;
     
     
@@ -22,7 +22,7 @@ namespace Word_Guss_2._1.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    internal class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,10 +36,10 @@ namespace Word_Guss_2._1.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
+        internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Word_Guss_2._1.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Word_Guss_2.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -51,7 +51,7 @@ namespace Word_Guss_2._1.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
+        internal static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace Word_Guss_2._1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap p1 {
+        internal static System.Drawing.Bitmap p1 {
             get {
                 object obj = ResourceManager.GetObject("p1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -73,7 +73,7 @@ namespace Word_Guss_2._1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap p2 {
+        internal static System.Drawing.Bitmap p2 {
             get {
                 object obj = ResourceManager.GetObject("p2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -83,7 +83,7 @@ namespace Word_Guss_2._1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap p3 {
+        internal static System.Drawing.Bitmap p3 {
             get {
                 object obj = ResourceManager.GetObject("p3", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -93,7 +93,7 @@ namespace Word_Guss_2._1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap p4 {
+        internal static System.Drawing.Bitmap p4 {
             get {
                 object obj = ResourceManager.GetObject("p4", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -103,7 +103,7 @@ namespace Word_Guss_2._1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap p5 {
+        internal static System.Drawing.Bitmap p5 {
             get {
                 object obj = ResourceManager.GetObject("p5", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -113,7 +113,7 @@ namespace Word_Guss_2._1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap p6 {
+        internal static System.Drawing.Bitmap p6 {
             get {
                 object obj = ResourceManager.GetObject("p6", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
