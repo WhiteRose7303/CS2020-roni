3e67e89566be3cf93bad3ec9c9e6c246c092113b
hadar ovadia <hadarov@tichonet.com>
2020-12-28 21:56:54 +0200

    simon is done

--- Simon/Form1.Designer.cs
+++ Simon/Form1.Designer.cs
@@ -32,14 +32,16 @@
             this.Start = new System.Windows.Forms.Button();
             this.Exit = new System.Windows.Forms.Button();
             this.Game = new System.Windows.Forms.Timer(this.components);
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.P4 = new System.Windows.Forms.PictureBox();
+            this.P3 = new System.Windows.Forms.PictureBox();
+            this.P2 = new System.Windows.Forms.PictureBox();
+            this.P1 = new System.Windows.Forms.PictureBox();
+            this.Delay = new System.Windows.Forms.Timer(this.components);
+            this.Stage = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.P4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P1)).BeginInit();
             this.SuspendLayout();
             // 
             // Start
@@ -64,48 +66,66 @@
             // 
             // Game
             // 
-            this.Game.Interval = 1000;
+            this.Game.Interval = 500;
             this.Game.Tick += new System.EventHandler(this.Game_Tick);
             // 
-            // pictureBox4
+            // P4
             // 
-            this.pictureBox4.Image = global::Simon.Properties.Resources.karnaf;
-            this.pictureBox4.Location = new System.Drawing.Point(344, 265);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(234, 189);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 5;
-            this.pictureBox4.TabStop = false;
+            this.P4.Image = global::Simon.Properties.Resources.karnaf;
+            this.P4.Location = new System.Drawing.Point(344, 265);
+            this.P4.Name = "P4";
+            this.P4.Size = new System.Drawing.Size(234, 189);
+            this.P4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.P4.TabIndex = 5;
+            this.P4.TabStop = false;
+            this.P4.Click += new System.EventHandler(this.pictureBox_Click);
             // 
-            // pictureBox3
+            // P3
             // 
-            this.pictureBox3.Image = global::Simon.Properties.Resources.lion;
-            this.pictureBox3.Location = new System.Drawing.Point(12, 265);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(234, 189);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 4;
-            this.pictureBox3.TabStop = false;
+            this.P3.Image = global::Simon.Properties.Resources.lion;
+            this.P3.Location = new System.Drawing.Point(12, 265);
+            this.P3.Name = "P3";
+            this.P3.Size = new System.Drawing.Size(234, 189);
+            this.P3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.P3.TabIndex = 4;
+            this.P3.TabStop = false;
+            this.P3.Click += new System.EventHandler(this.pictureBox_Click);
             // 
-            // pictureBox2
+            // P2
             // 
-            this.pictureBox2.Image = global::Simon.Properties.Resources.zebra;
-            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(234, 189);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.P2.Image = global::Simon.Properties.Resources.zebra;
+            this.P2.Location = new System.Drawing.Point(12, 12);
+            this.P2.Name = "P2";
+            this.P2.Size = new System.Drawing.Size(234, 189);
+            this.P2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.P2.TabIndex = 3;
+            this.P2.TabStop = false;
+            this.P2.Click += new System.EventHandler(this.pictureBox_Click);
             // 
-            // pictureBox1
+            // P1
             // 
-            this.pictureBox1.Image = global::Simon.Properties.Resources.fish;
-            this.pictureBox1.Location = new System.Drawing.Point(344, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(234, 189);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.P1.Image = global::Simon.Properties.Resources.fish;
+            this.P1.Location = new System.Drawing.Point(344, 12);
+            this.P1.Name = "P1";
+            this.P1.Size = new System.Drawing.Size(234, 189);
+            this.P1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.P1.TabIndex = 2;
+            this.P1.TabStop = false;
+            this.P1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // Delay
+            // 
+            this.Delay.Interval = 350;
+            this.Delay.Tick += new System.EventHandler(this.Delay_Tick);
+            // 
+            // Stage
+            // 
+            this.Stage.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Stage.Location = new System.Drawing.Point(248, 129);
+            this.Stage.Name = "Stage";
+            this.Stage.Size = new System.Drawing.Size(94, 20);
+            this.Stage.TabIndex = 6;
+            this.Stage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
@@ -113,20 +133,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(590, 466);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Stage);
+            this.Controls.Add(this.P4);
+            this.Controls.Add(this.P3);
+            this.Controls.Add(this.P2);
+            this.Controls.Add(this.P1);
             this.Controls.Add(this.Exit);
             this.Controls.Add(this.Start);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -134,11 +156,13 @@
 
         private System.Windows.Forms.Button Start;
         private System.Windows.Forms.Button Exit;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox P1;
+        private System.Windows.Forms.PictureBox P2;
+        private System.Windows.Forms.PictureBox P3;
+        private System.Windows.Forms.PictureBox P4;
         private System.Windows.Forms.Timer Game;
+        private System.Windows.Forms.Timer Delay;
+        private System.Windows.Forms.TextBox Stage;
     }
 }
 
--- Simon/Form1.cs
+++ Simon/Form1.cs
@@ -15,14 +15,31 @@ namespace Simon
         public Form1()
         {
             InitializeComponent();
-            Brnd();
+            
         }
-        int Pmount = 1;
-        int last = 0;
-        private static void Brnd()
+        //the Amount in this turn
+        int AmountToPlay = 1;
+        //the last posion in the array
+        int lastPos = 0;
+        int played = 0;
+        int[] rndArray;
+        string[] playarr;
+        int turn = 0;
+        int turns;
+        
+        private int calcstage(int n)
+        {
+            int fact = 0;
+            for (int i = 0; i<=n; i++)
+            {
+                fact += i;
+            }
+            return fact;
+        }
+
+        private void Brnd()
         {
             Random rnd = new Random();
-            int[] rndArray = new int[1000];
             for (int i = 0; i< rndArray.Length; i++)
             {
                 rndArray[i] = rnd.Next(1, 4);
@@ -36,12 +53,111 @@ namespace Simon
 
         private void Start_Click(object sender, EventArgs e)
         {
+            turns = int.Parse(Stage.Text);
+            rndArray = new int[calcstage(turns)];
+            Stage.Enabled = false;
+            Brnd();
             Game.Start();
+            playarr = new string[AmountToPlay];
         }
 
         private void Game_Tick(object sender, EventArgs e)
         {
+            if (turn != turns)
+            {
+                if (played != AmountToPlay)
+                {
+                    switch (rndArray[lastPos])
+                    {
+                        case 1:
+                            P1.Image = Properties.Resources.fishO;
+                            Delay.Start();
+                            lastPos++;
+                            played++;
+                            playarr[played - 1] = "P1";
+                            break;
+                        case 2:
+                            P2.Image = Properties.Resources.zebraO;
+                            Delay.Start();
+                            lastPos++;
+                            played++;
+                            playarr[played - 1] = "P2";
+                            break;
+                        case 3:
+                            P3.Image = Properties.Resources.lionO;
+                            Delay.Start();
+                            lastPos++;
+                            played++;
+                            playarr[played - 1] = "P3";
+                            break;
+                        case 4:
+                            P4.Image = Properties.Resources.karnafO;
+                            Delay.Start();
+                            lastPos++;
+                            played++;
+                            playarr[played - 1] = "P4";
+                            break;
+                    }
+                }
+                else
+                {
+                    Game.Stop();
+                    now();
+                }
+            }
+            else
+            {
+                Game.Stop();
+                MessageBox.Show("You Won!");
+                this.Close();
+            }
+        }
+        private void now()
+        {
+            AmountToPlay++;
+            played = 0;
+        }
 
+        private void Delay_Tick(object sender, EventArgs e)
+        {
+            P1.Image = Properties.Resources.fish;
+            P2.Image = Properties.Resources.zebra;
+            P3.Image = Properties.Resources.lion;
+            P4.Image = Properties.Resources.karnaf;
+            Delay.Stop();
+        }
+
+        int haveclicked = 0;
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            string Clicked = (sender as PictureBox).Name;
+            if (haveclicked != playarr.Length)
+            {
+                if (Clicked != playarr[haveclicked])
+                {
+                    MessageBox.Show("Looser!");
+                    this.Close();
+                }
+                else
+                {
+                    haveclicked++;
+                    if (haveclicked == playarr.Length)
+                    {
+                        haveclicked = 0;
+                        Game.Start();
+                        playarr = new string[AmountToPlay];
+                        turn++;
+                    }
+                }
+            }
+            else
+            {
+                haveclicked = 0;
+                Game.Start();
+                playarr = new string[AmountToPlay];
+                turn++;
+            }
         }
     }
 }
